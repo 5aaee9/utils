e5da98fd7fc9f1c426dd4da108b188f8aa24aca7
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2024-04-21 00:27:19 +0800

    fix: darwin system proxy in user mode

--- pkgs/proxy/sysproxy/sysproxy_test.go
+++ pkgs/proxy/sysproxy/sysproxy_test.go
@@ -13,3 +13,16 @@ func Test_SystemProxyStatus(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, status)
 }
+
+// func Test_CloseSystemProxy(t *testing.T) {
+// 	proxy := NewSystemProxy()
+// 	err := proxy.TurnOff()
+// 	assert.NoError(t, err)
+// }
+
+// func Test_OpenSystemProxy(t *testing.T) {
+// 	proxy := NewSystemProxy()
+// 	err := proxy.TurnOn("127.0.0.1:9090")
+
+// 	assert.NoError(t, err)
+// }
